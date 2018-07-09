5bf756e4b4a1c841d0569030d8d950940c1a552c
Hasstrup <31807665+Hasstrup@users.noreply.github.com>
2018-07-09 07:23:17 +0100

    Fixes socket connection

--- services/real-time-service/interactors/index.js
+++ services/real-time-service/interactors/index.js
@@ -1,9 +1,9 @@
 import PostService from './internal/exec';
 
 const dispatchToDb = (data) => {
-  const arr = [PostService.internalCreate, PostService.externalCreate]
-  const index = Math.floor(Math.random() * 1)
+  const arr = [PostService.internalCreate, PostService.externalCreate];
+  const index = Math.floor(Math.random() * 2);
   return arr[index](data);
-}
+};
 
 export default dispatchToDb;
